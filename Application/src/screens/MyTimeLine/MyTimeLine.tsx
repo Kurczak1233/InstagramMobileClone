8f54d40d14969e5d76df473d87856558ea4d7087
Michal Kupczak <kurczak12333@gmail.com>
2022-11-21 22:26:58 +0100

    feat: Implemented template for my page

--- Application/src/screens/MyTimeLine/MyTimeLine.tsx
+++ Application/src/screens/MyTimeLine/MyTimeLine.tsx
@@ -1,9 +1,20 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
+
+import { UserAvatar } from "../../components/UserAvatar";
+import Header from "../../components/typography/Header";
+import { styles } from "./styles";
 
 export const MyTimeLine = () => {
   return (
-    <View>
-      <Text>MyTimeLine</Text>
+    <View style={styles.container}>
+      <UserAvatar
+        imageSize="large"
+        showUserName={false}
+        imageUrl="https://preview.redd.it/vlxyi9pntl541.jpg?width=640&crop=smart&auto=webp&s=901cdbfe9cce6f4e23ec3150b7ed07a1660401a1"
+      />
+      <View style={styles.myName}>
+        <Header variant="h5">Me</Header>
+      </View>
     </View>
   );
 };
---
+++ Application/src/screens/MyTimeLine/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+import theme from "../../theme/theme";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: theme.spacing.lg,
+    alignItems: "center",
+  },
+  myName: {
+    padding: theme.spacing.md,
+  },
+});
