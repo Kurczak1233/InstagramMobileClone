be8499206b59a8411ef32b5275ca6e0333be1bf0
Michal Kupczak <kurczak12333@gmail.com>
2022-11-24 23:56:21 +0100

    fix: Fixed looping animation

--- Application/src/screens/IntroductionScreen/IntroductionScreen.tsx
+++ Application/src/screens/IntroductionScreen/IntroductionScreen.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import type { StackNavigationProp } from "@react-navigation/stack";
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Button, Image } from "react-native";
 import Animated, {
   useSharedValue,
@@ -24,6 +24,10 @@ export const IntroductionScreen = () => {
     };
   });
 
+  useEffect(() => {
+    rotation.value = withRepeat(withTiming(10), Infinity, true);
+  }, []);
+
   return (
     <View style={styles.welcomeContainer}>
       <Header variant="h3" color={theme.colors.primary}>
@@ -35,12 +39,6 @@ export const IntroductionScreen = () => {
           onPress={() => navigation.navigate("Login")}
         />
       </View>
-      <Button
-        onPress={() => {
-          rotation.value = withRepeat(withTiming(10), Infinity, true);
-        }}
-        title="Move"
-      />
       <Image
         source={{
           uri: "https://zapodaj.net/images/cf365ec44b467.png",
